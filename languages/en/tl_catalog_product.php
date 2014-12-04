ba97f6632e731dee08b9c212fd6df2a154dd35e5
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2014-12-04 12:29:25 +0330

    add some language terms

--- languages/en/tl_catalog_product.php
+++ languages/en/tl_catalog_product.php
@@ -19,17 +19,21 @@ $GLOBALS['TL_LANG']['tl_catalog_product']['title']       = array('Product title'
 $GLOBALS['TL_LANG']['tl_catalog_product']['alias']       = array('Product code or alias', 'Please enter the product code.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['price']       = array('Price', 'Please enter the price in Iraniain Rials.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['date']        = array('Date', 'Please enter date.');
+$GLOBALS['TL_LANG']['tl_catalog_product']['description'] = array('Description','Please enter description.');
+$GLOBALS['TL_LANG']['tl_catalog_product']['keywords']    = array('Meta keywords','Here you can enter a list of comma separated keywords. Keywords, however, are no longer relevant to most search engines (including Google).');
 $GLOBALS['TL_LANG']['tl_catalog_product']['tableitems']  = array('Table items', 'If JavaScript is disabled, make sure to save your changes before modifying the order.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['summary']     = array('Table summary', 'Please enter a short summary of the table and describe its purpose or structure.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['thead']       = array('Add table header', 'Make the first row of the table the table header.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['tfoot']       = array('Add table footer', 'Make the last row of the table the table footer.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['tleft']       = array('Use row headers', 'Define the left column as row header.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['spec']        = array('Specifications','Please eneter the product specifications.');
+$GLOBALS['TL_LANG']['tl_catalog_product']['features']    = array('Features','Please eneter the product features.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['spectitle']   = array('Title','Specifications title');
 $GLOBALS['TL_LANG']['tl_catalog_product']['specvalue']   = array('Value','Specifications value');
 $GLOBALS['TL_LANG']['tl_catalog_product']['addImage']    = array('Add an image', 'Add an image to the category item.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['singleSRC']   = array('Source file','Please select an image.');
-$GLOBALS['TL_LANG']['tl_catalog_product']['description'] = array('Description','Please enter description.');
+$GLOBALS['TL_LANG']['tl_catalog_product']['addEnclosure']= array('Add enclosures', 'Add one or more downloadable files to the news item.');
+$GLOBALS['TL_LANG']['tl_catalog_product'][ 'enclosure']  = array('Enclosures', 'Please choose the files you want to attach.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['protected']   = array('Protect product','Show product items to certain member groups only.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['groups']      = array('Allowed member groups','');
 $GLOBALS['TL_LANG']['tl_catalog_product']['published']   = array('Publish product','Make the product item publicly visible on the website.');
@@ -37,12 +41,14 @@ $GLOBALS['TL_LANG']['tl_catalog_product']['featured']    = array('Feature produc
 $GLOBALS['TL_LANG']['tl_catalog_product']['start']       = array('Show from','Do not show the product item on the website before this day.');
 $GLOBALS['TL_LANG']['tl_catalog_product']['stop']        = array('Show until','Do not show the news product on the website on and after this day.');
 
-
 /**
  * Legends
  */
 $GLOBALS['TL_LANG']['tl_catalog_product']['title_legend']       = 'Product title';
-$GLOBALS['TL_LANG']['tl_catalog_product']['spec_legend']        = 'Other specifications';
+$GLOBALS['TL_LANG']['tl_catalog_product']['meta_legend']        = 'Meta information for SEO';
+$GLOBALS['TL_LANG']['tl_catalog_product']['spec_legend']       = 'Other specifications';
+$GLOBALS['TL_LANG']['tl_catalog_product']['feature_legend']     = 'Feature specifications';
+$GLOBALS['TL_LANG']['tl_catalog_product']['enclosure_legend']   = 'Enclosures';
 $GLOBALS['TL_LANG']['tl_catalog_product']['image_legend']       = 'Image settings';
 $GLOBALS['TL_LANG']['tl_catalog_product']['table_legend']       = 'Specifications table';
 $GLOBALS['TL_LANG']['tl_catalog_product']['description_legend'] = 'Description';
