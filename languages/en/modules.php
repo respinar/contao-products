d0d0786dfb401b5ca9095017a8c86bdad59d359b
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2015-06-18 00:23:18 +0430

    Correcting language arrays

--- languages/en/modules.php
+++ languages/en/modules.php
@@ -3,27 +3,24 @@
 /**
  * Contao Open Source CMS
  *
- * Copyright (c) 2005-2013 Leo Feyer
+ * Copyright (c) 2005-2015 Leo Feyer
  *
  * @package   catalog
  * @author    Hamid Abbaszadeh
  * @license   GNU/LGPL
- * @copyright 2014
+ * @copyright 2014-2015
  */
 
 
 /**
  * Back end modules
  */
-$GLOBALS['TL_LANG']['MOD']['catalog']['0'] = 'Catalog';
-$GLOBALS['TL_LANG']['MOD']['catalog']['1'] = 'Catalog and products management';
+$GLOBALS['TL_LANG']['MOD']['catalog']        = array('Catalog','Catalog and products management');
 
 
 /**
  * Front end modules
  */
-$GLOBALS['TL_LANG']['FMD']['catalog']             = 'Catalog';
-$GLOBALS['TL_LANG']['FMD']['catalog_list']['0']   = 'Product list';
-$GLOBALS['TL_LANG']['FMD']['catalog_list']['1']   = 'Product list';
-$GLOBALS['TL_LANG']['FMD']['catalog_detail']['0'] = 'Product detail';
-$GLOBALS['TL_LANG']['FMD']['catalog_detail']['1'] = 'Product detail';
+$GLOBALS['TL_LANG']['FMD']['catalog']        = 'Catalog';
+$GLOBALS['TL_LANG']['FMD']['catalog_list']   = array('Product list','Product list');
+$GLOBALS['TL_LANG']['FMD']['catalog_detail'] = array('Product detail','Product detail');
--- languages/en/tl_catalog_category.php
+++ languages/en/tl_catalog_category.php
@@ -3,30 +3,24 @@
 /**
  * Contao Open Source CMS
  *
- * Copyright (c 2005-2013 Leo Feyer
+ * Copyright (c) 2005-2015 Leo Feyer
  *
  * @package   catalog
  * @author    Hamid Abbaszadeh
  * @license   GNU/LGPL
- * @copyright 2014
+ * @copyright 2014-2015
  */
 
 
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_catalog_category']['title']['0']     = 'Category Title';
-$GLOBALS['TL_LANG']['tl_catalog_category']['title']['1']     = 'Please enter the category title.';
-$GLOBALS['TL_LANG']['tl_catalog_category']['jumpTo']['0']    = 'Redirect page';
-$GLOBALS['TL_LANG']['tl_catalog_category']['jumpTo']['1']    = 'Please choose the list page to which visitors will be redirected when clicking a menu.';
-$GLOBALS['TL_LANG']['tl_catalog_category']['protected']['0'] = 'Protect category';
-$GLOBALS['TL_LANG']['tl_catalog_category']['protected']['1'] = 'Show category items to certain member groups only.';
-$GLOBALS['TL_LANG']['tl_catalog_category']['groups']['0']    = 'Allowed member groups';
-$GLOBALS['TL_LANG']['tl_catalog_category']['groups']['1']    = 'These groups will be able to see the menu items in this catalog.';
-$GLOBALS['TL_LANG']['tl_catalog_category']['master']['0']    = 'Master category';
-$GLOBALS['TL_LANG']['tl_catalog_category']['master']['1']    = 'Please define the master category to allow language switching.';
-$GLOBALS['TL_LANG']['tl_catalog_category']['language']['0']  = 'Language';
-$GLOBALS['TL_LANG']['tl_catalog_category']['language']['1']  = 'Please enter the language according to the RFC3066 format (e.g. en, en-us or en-cockney.';
+$GLOBALS['TL_LANG']['tl_catalog_category']['title']     = array('Category Title', 'Please enter the category title.');
+$GLOBALS['TL_LANG']['tl_catalog_category']['jumpTo']    = array('Redirect page','Please choose the list page to which visitors will be redirected when clicking a menu.');
+$GLOBALS['TL_LANG']['tl_catalog_category']['protected'] = array('Protect category','Show category items to certain member groups only.');
+$GLOBALS['TL_LANG']['tl_catalog_category']['groups']    = array('Allowed member groups','These groups will be able to see the menu items in this catalog.');
+$GLOBALS['TL_LANG']['tl_catalog_category']['master']    = array('Master category','Please define the master category to allow language switching.');
+$GLOBALS['TL_LANG']['tl_catalog_category']['language']  = array('Language','Please enter the language according to the RFC3066 format (e.g. en, en-us or en-cockney.');
 
 
 /**
@@ -48,15 +42,8 @@ $GLOBALS['TL_LANG']['tl_catalog_category']['protected_legend'] = 'Access protect
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_catalog_category']['new']['0']    = 'New category';
-$GLOBALS['TL_LANG']['tl_catalog_category']['new']['1']    = 'Create a new category';
-$GLOBALS['TL_LANG']['tl_catalog_category']['show']['0']   = 'Category details';
-$GLOBALS['TL_LANG']['tl_catalog_category']['show']['1']   = 'Show the details of category ID %s';
-$GLOBALS['TL_LANG']['tl_catalog_category']['edit']['0']   = 'Edit category';
-$GLOBALS['TL_LANG']['tl_catalog_category']['edit']['1']   = 'Edit category ID %s';
-$GLOBALS['TL_LANG']['tl_catalog_category']['cut']['0']    = 'Move category';
-$GLOBALS['TL_LANG']['tl_catalog_category']['cut']['1']    = 'Move category ID %s';
-$GLOBALS['TL_LANG']['tl_catalog_category']['copy']['0']   = 'Duplicate category';
-$GLOBALS['TL_LANG']['tl_catalog_category']['copy']['1']   = 'Duplicate category ID %s';
-$GLOBALS['TL_LANG']['tl_catalog_category']['delete']['0'] = 'Delete category';
-$GLOBALS['TL_LANG']['tl_catalog_category']['delete']['1'] = 'Delete category ID %s';
+$GLOBALS['TL_LANG']['tl_catalog_category']['new']    = array('New category','Create a new category');
+$GLOBALS['TL_LANG']['tl_catalog_category']['show']   = array('Category details','Show the details of category ID %s');
+$GLOBALS['TL_LANG']['tl_catalog_category']['edit']   = array('Edit category','Edit category ID %s');
+$GLOBALS['TL_LANG']['tl_catalog_category']['copy']   = array('Duplicate category','Duplicate category ID %s');
+$GLOBALS['TL_LANG']['tl_catalog_category']['delete'] = array('Delete category', 'Delete category ID %s');
