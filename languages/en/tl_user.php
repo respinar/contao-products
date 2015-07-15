d1145e65af8f274caf9e5ce50f57c07288b40b19
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2015-07-15 14:12:33 +0430

    Add permissions's language strings

---
+++ languages/en/tl_user.php
@@ -0,0 +1,25 @@
+<?php
+	
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   catalog
+ * @author    Hamid Abbaszadeh
+ * @license   LGPL-3.0+
+ * @copyright 2014-2015
+ */
+ 
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_user']['catalogs'] = array('Allowed Catalog categories', 'Here you can grant access to one or more Catalog categories.');
+$GLOBALS['TL_LANG']['tl_user']['catalogp'] = array('Catalog category permissions', 'Here you can define the Catalog category permissions.');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_user']['catalog_legend']     = 'Catalog permissions';
---
+++ languages/en/tl_user_group.php
@@ -0,0 +1,18 @@
+<?php
+	
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   atheletes
+ * @author    Hamid Abbaszadeh
+ * @license   LGPL-3.0+
+ * @copyright 2014-2015
+ */
+ 
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_user_group']['catalog_legend'] = 'Catalog permissions';
