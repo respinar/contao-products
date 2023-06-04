87e107aaec2b9e85d1b4de9671f1f71d1c1bf381
Hamid Paywasti <abbaszadeh.h@gmail.com>
2023-06-04 20:27:16 +0330

    Adding user permissions

---
+++ contao/languages/fa/tl_user.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-product/language/fa/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2016-04-15T09:34:13+00:00
+ */
+
+$GLOBALS['TL_LANG']['tl_user']['product_legend'] = 'مجوزهای محصول';
+$GLOBALS['TL_LANG']['tl_user']['productp']['0']  = 'مجوزهای کاتالوگ محصول';
+$GLOBALS['TL_LANG']['tl_user']['productp']['1']  = 'اینجا می‌توانید مجوزهای کاتالوگ محصول را تعریف نمایید.';
+$GLOBALS['TL_LANG']['tl_user']['products']['0']  = 'کاتالوگ‌های مجاز';
+
