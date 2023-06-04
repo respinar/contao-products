b48779425e84c1e1d5b5235ce16dabb4587f0c10
Hamid Paywasti <abbaszadeh.h@gmail.com>
2023-06-04 20:28:05 +0330

    Adding translations

---
+++ contao/languages/en/tl_module.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @package   product
+ * @author    Hamid Abbaszadeh
+ * @license   LGPL-3.0+
+ * @copyright 2014-2016
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_module']['product_catalogs']     = array('Product catelogs','Please select one or more catalogs.');
+$GLOBALS['TL_LANG']['tl_module']['product_categories']   = array('Product categories','Please select one or more categories.');
+$GLOBALS['TL_LANG']['tl_module']['product_featured']     = array('Featured product','Here you can choose how featured products are handled.');
+$GLOBALS['TL_LANG']['tl_module']['product_detailModule'] = array('Product detail module','Automatically switch to the product module if an item has been selected.');
+$GLOBALS['TL_LANG']['tl_module']['product_metaFields']   = array('Meta fields','Here you can select the meta fields.');
+$GLOBALS['TL_LANG']['tl_module']['product_sortBy']       = array('Sort by','Here you can select the sorting fields.');
+$GLOBALS['TL_LANG']['tl_module']['product_template']     = array('Product template','Here you can select the product template.');
+$GLOBALS['TL_LANG']['tl_module']['product_Class']        = array('Product CSS class','Here you can set one or more classes for products.');
+$GLOBALS['TL_LANG']['tl_module']['product_list_Class']   = array('Product list class','Here you can set one or more classes for products list.');
+$GLOBALS['TL_LANG']['tl_module']['related_show']         = array('Show related product','Here you can set one or more classes for types.');
+$GLOBALS['TL_LANG']['tl_module']['related_template']     = array('Related product template','Here you can select the related product template.');
+$GLOBALS['TL_LANG']['tl_module']['related_Class']        = array('Related product CSS class','Here you can set one or more classes for related products.');
+$GLOBALS['TL_LANG']['tl_module']['related_imgSize']      = array('Related product image width and height','Here you can set the image dimensions and the resize mode.');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_module']['catalog_legend']   = 'Catalog';
+$GLOBALS['TL_LANG']['tl_module']['product_legend']   = 'Product setting';
+$GLOBALS['TL_LANG']['tl_module']['related_legend']   = 'Related products settings';
+$GLOBALS['TL_LANG']['tl_module']['protected_legend'] = 'Access protection';
+
+
+/**
+ *
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['all_product']         = 'All products';
+$GLOBALS['TL_LANG']['tl_module']['featured_product']    = 'Featured products';
+$GLOBALS['TL_LANG']['tl_module']['unfeatured_product']  = 'Unfeatured products';
+
+$GLOBALS['TL_LANG']['tl_module']['custom']     = 'Custom order';
+$GLOBALS['TL_LANG']['tl_module']['title_asc']  = 'Title (A-Z)';
+$GLOBALS['TL_LANG']['tl_module']['title_desc'] = 'Title (Z-A)';
+$GLOBALS['TL_LANG']['tl_module']['date_asc']   = 'Date (Older)';
+$GLOBALS['TL_LANG']['tl_module']['date_desc']  = 'Date (Newer)';
