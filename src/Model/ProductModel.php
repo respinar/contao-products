799abf252a4206d618545738ebc3f9146b672895
Hamid Paywasti <abbaszadeh.h@gmail.com>
2023-06-04 20:28:31 +0330

    Add Models

---
+++ src/Model/ProductModel.php
@@ -0,0 +1,355 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @package   product
+ * @author    Hamid Abbaszadeh
+ * @license   LGPL-3.0+
+ * @copyright 2014-2016
+ */
+
+
+/**
+ * Namespace
+ */
+namespace Respinar\ProductsBundle\Model;
+
+use Contao\Model;
+
+/**
+ * Class ProductModel
+ *
+ * @copyright  2014
+ * @author     Hamid Abbaszadeh
+ * @package    Devtools
+ */
+class ProductModel extends Model
+{
+
+	/**
+	 * Name of the table
+	 * @var string
+	 */
+	protected static $strTable = 'tl_product';
+
+
+	/**
+	 * Find published product items by their parent ID and ID or alias
+	 *
+	 * @param mixed $varId      The numeric ID or alias name
+	 * @param array $arrPids    An array of parent IDs
+	 * @param array $arrOptions An optional options array
+	 *
+	 * @return Model|null The productModel or null if there are no product
+	 */
+	public static function findPublishedByIdOrAlias($varId, array $arrOptions=array())
+	{
+
+		$t = static::$strTable;
+		$arrColumns = array("($t.id=? OR $t.alias=?)");
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		return static::findBy($arrColumns, array((is_numeric($varId) ? $varId : 0), $varId), $arrOptions);
+	}
+
+
+	/**
+	 * Find published product items by their parent ID and ID or alias
+	 *
+	 * @param mixed $varId      The numeric ID or alias name
+	 * @param array $arrPids    An array of parent IDs
+	 * @param array $arrOptions An optional options array
+	 *
+	 * @return \Model|null The productModel or null if there are no product
+	 */
+	public static function findPublishedByParentAndIdOrAlias($varId, $arrPids, array $arrOptions=array())
+	{
+		if (!is_array($arrPids) || empty($arrPids))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("($t.id=? OR $t.alias=?) AND $t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		return static::findBy($arrColumns, array((is_numeric($varId) ? $varId : 0), $varId), $arrOptions);
+	}
+
+
+	/**
+	 * Find published product items by their parent ID
+	 *
+	 * @param array   $arrPids     An array of product catalogs IDs
+	 * @param boolean $blnFeatured If true, return only featured product, if false, return only unfeatured product
+	 * @param integer $intLimit    An optional limit
+	 * @param integer $intOffset   An optional offset
+	 * @param array   $arrOptions  An optional options array
+	 *
+	 * @return \Model\Collection|null A collection of models or null if there are no product
+	 */
+	public static function findPublishedByIds($arrIds, $blnFeatured=null, $intLimit=0, $intOffset=0, array $arrOptions=array())
+	{
+		if (!is_array($arrIds) || empty($arrIds))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("$t.id IN(" . implode(',', array_map('intval', $arrIds)) . ")");
+
+		if ($blnFeatured === true)
+		{
+			$arrColumns[] = "$t.featured=1";
+		}
+		elseif ($blnFeatured === false)
+		{
+			$arrColumns[] = "$t.featured=''";
+		}
+
+		// Never return unpublished elements in the back end, so they don't end up in the RSS feed
+		if (!BE_USER_LOGGED_IN || TL_MODE == 'BE')
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order']  = "$t.date DESC";
+		}
+
+		$arrOptions['limit']  = $intLimit;
+		$arrOptions['offset'] = $intOffset;
+
+		return static::findBy($arrColumns, null, $arrOptions);
+	}
+
+	/**
+	 * Find published product items by their parent ID
+	 *
+	 * @param array   $arrPids     An array of product catalogs IDs
+	 * @param boolean $blnFeatured If true, return only featured product, if false, return only unfeatured product
+	 * @param integer $intLimit    An optional limit
+	 * @param integer $intOffset   An optional offset
+	 * @param array   $arrOptions  An optional options array
+	 *
+	 * @return \Model\Collection|null A collection of models or null if there are no product
+	 */
+	public static function findPublishedByPids($arrPids, $blnFeatured=null, $intLimit=0, $intOffset=0, array $arrOptions=array())
+	{
+		if (!is_array($arrPids) || empty($arrPids))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("$t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+
+		if ($blnFeatured === true)
+		{
+			$arrColumns[] = "$t.featured=1";
+		}
+		elseif ($blnFeatured === false)
+		{
+			$arrColumns[] = "$t.featured=''";
+		}
+
+		// Never return unpublished elements in the back end, so they don't end up in the RSS feed
+		if (!BE_USER_LOGGED_IN || TL_MODE == 'BE')
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order']  = "$t.date DESC";
+		}
+
+		$arrOptions['limit']  = $intLimit;
+		$arrOptions['offset'] = $intOffset;
+
+		return static::findBy($arrColumns, null, $arrOptions);
+	}
+
+
+	/**
+	 * Count published product items by their parent ID
+	 *
+	 * @param array   $arrPids     An array of product catalogs IDs
+	 * @param boolean $blnFeatured If true, return only featured product, if false, return only unfeatured product
+	 * @param array   $arrOptions  An optional options array
+	 *
+	 * @return integer The number of product items
+	 */
+	public static function countPublishedByPids($arrPids, $blnFeatured=null, array $arrOptions=array())
+	{
+		if (!is_array($arrPids) || empty($arrPids))
+		{
+			return 0;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("$t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+
+		if ($blnFeatured === true)
+		{
+			$arrColumns[] = "$t.featured=1";
+		}
+		elseif ($blnFeatured === false)
+		{
+			$arrColumns[] = "$t.featured=''";
+		}
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		return static::countBy($arrColumns, null, $arrOptions);
+	}
+
+
+	/**
+	 * Find published product items with the default redirect target by their parent ID
+	 *
+	 * @param integer $intPid     The product catalogs ID
+	 * @param array   $arrOptions An optional options array
+	 *
+	 * @return \Model\Collection|null A collection of models or null if there are no product
+	 */
+	public static function findPublishedDefaultByPid($intPid, array $arrOptions=array())
+	{
+		$t = static::$strTable;
+		$arrColumns = array("$t.pid=?");
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order'] = "$t.date DESC";
+		}
+
+		return static::findBy($arrColumns, $intPid, $arrOptions);
+	}
+
+
+	/**
+	 * Find published product items by their parent ID
+	 *
+	 * @param integer $intId      The product catalogs ID
+	 * @param integer $intLimit   An optional limit
+	 * @param array   $arrOptions An optional options array
+	 *
+	 * @return \Model\Collection|null A collection of models or null if there are no product
+	 */
+	public static function findPublishedByPid($intId, $intLimit=0, array $arrOptions=array())
+	{
+		$time = time();
+		$t = static::$strTable;
+
+		$arrColumns = array("$t.pid=? AND ($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1");
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order'] = "$t.date DESC";
+		}
+
+		if ($intLimit > 0)
+		{
+			$arrOptions['limit'] = $intLimit;
+		}
+
+		return static::findBy($arrColumns, $intId, $arrOptions);
+	}
+
+
+	/**
+	 * Find all published product items of a certain period of time by their parent ID
+	 *
+	 * @param integer $intFrom    The start date as Unix timestamp
+	 * @param integer $intTo      The end date as Unix timestamp
+	 * @param array   $arrPids    An array of product catalogs IDs
+	 * @param integer $intLimit   An optional limit
+	 * @param integer $intOffset  An optional offset
+	 * @param array   $arrOptions An optional options array
+	 *
+	 * @return \Model\Collection|null A collection of models or null if there are no product
+	 */
+	public static function findPublishedFromToByPids($intFrom, $intTo, $arrPids, $intLimit=0, $intOffset=0, array $arrOptions=array())
+	{
+		if (!is_array($arrPids) || empty($arrPids))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("$t.date>=? AND $t.date<=? AND $t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order']  = "$t.date DESC";
+		}
+
+		$arrOptions['limit']  = $intLimit;
+		$arrOptions['offset'] = $intOffset;
+
+		return static::findBy($arrColumns, array($intFrom, $intTo), $arrOptions);
+	}
+
+
+	/**
+	 * Count all published product items of a certain period of time by their parent ID
+	 *
+	 * @param integer $intFrom    The start date as Unix timestamp
+	 * @param integer $intTo      The end date as Unix timestamp
+	 * @param array   $arrPids    An array of product catalogs IDs
+	 * @param array   $arrOptions An optional options array
+	 *
+	 * @return integer The number of product items
+	 */
+	public static function countPublishedFromToByPids($intFrom, $intTo, $arrPids, array $arrOptions=array())
+	{
+		if (!is_array($arrPids) || empty($arrPids))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("$t.date>=? AND $t.date<=? AND $t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		return static::countBy($arrColumns, array($intFrom, $intTo), $arrOptions);
+	}
+
+}
