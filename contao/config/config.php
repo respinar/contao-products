cc6c7a1131cd17dd99fa3746575c251712c26943
Hamid Peywasti <abbaszadeh.h@gmail.com>
2023-06-14 13:07:47 +0330

    add contao config

--- contao/config/config.php
+++ contao/config/config.php
@@ -11,58 +11,31 @@
  * @copyright 2014-2016
  */
 
+use Respinar\ProductsBundle\Model\ProductModel;
+use Respinar\ProductsBundle\Model\ProductCatalogModel;
 
 /**
  * Back end modules
  */
-array_insert($GLOBALS['BE_MOD'], 1, array
-(
-	'products' => array
-	(
-		'products' => array
-		(
-			'tables'     => array('tl_product_catalog','tl_product','tl_content')
-		)
-	)
-));
+$GLOBALS['BE_MOD']['products'] = array(
+		'products' => array(
+			'tables' => array('tl_product_catalog', 'tl_product', 'tl_content')
+			)
+		);
 
 /**
  * Register models
  */
-
- $GLOBALS['TL_MODELS']['tl_product']         = 'Respinar\Products\Model\ProductModel';
- $GLOBALS['TL_MODELS']['tl_product_catalog'] = 'Respinar\Products\Model\ProductCatalogModel'; 
- $GLOBALS['TL_MODELS']['tl_product_price']   = 'Respinar\Products\Model\ProductPriceModel'; 
-
-/**
- * Front end modules
- */
-
-array_insert($GLOBALS['FE_MOD'], 2, array
-(
-	'products' => array
-	(
-		'product_list'    => 'Respinar\Products\Frontend\Module\ModuleProductList',
-		'product_detail'  => 'Respinar\Products\Frontend\Module\ModuleProductDetail',
-	)
-));
-
-
-/**
- * Content elements
- */
-$GLOBALS['TL_CTE']['miscellaneous']['product']   = 'Respinar\Products\Frontend\Element\ContentProductSingle';
-$GLOBALS['TL_CTE']['miscellaneous']['products']  = 'Respinar\Products\Frontend\Element\ContentProductList';
-
+ $GLOBALS['TL_MODELS']['tl_product']         = ProductModel::class;
+ $GLOBALS['TL_MODELS']['tl_product_catalog'] = ProductCatalogModel::class; 
 
 /**
  * Register hook to add carpets items to the indexer
  */
-$GLOBALS['TL_HOOKS']['getSearchablePages'][] = array('Respinar\Products\Product', 'getSearchablePages');
-
+//$GLOBALS['TL_HOOKS']['getSearchablePages'][] = array('Respinar\Products\Product', 'getSearchablePages');
 
 // Registrieren im Hooks replaceInsertTags
-$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('Respinar\Products\Product', 'productURLInsertTags');
+//$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('Respinar\Products\Product', 'productURLInsertTags');
 
 /**
  * Add permissions
