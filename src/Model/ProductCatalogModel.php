8f8b58050a80a290e94d805fd29f620f62dddff5
Hamid Peywasti <abbaszadeh.h@gmail.com>
2023-06-14 13:07:18 +0330

    Add models

---
+++ src/Model/ProductCatalogModel.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @package   product
+ * @author    Hamid Abbaszadeh
+ * @license   LGPL-3.0+
+ * @copyright 2014-2016
+ */
+
+namespace Respinar\ProductsBUndle\Model;
+
+use Contao\Model;
+
+class ProductCatalogModel extends Model
+{
+
+	protected static $strTable = 'tl_product_catalog';
+
+}
