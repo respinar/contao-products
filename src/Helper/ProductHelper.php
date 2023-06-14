3a46cb82993a3fcaf7f919dd1a0745e7e3d21feb
Hamid Peywasti <abbaszadeh.h@gmail.com>
2023-06-14 23:12:24 +0330

    Bring more functionallty

--- src/Helper/ProductHelper.php
+++ src/Helper/ProductHelper.php
@@ -18,6 +18,10 @@
 namespace Respinar\ProductsBundle\Helper;
 
 use Contao\Config;
+use Contao\System;
+use Contao\StringUtil;
+use Contao\CoreBundle\Security\ContaoCorePermissions;
+use Respinar\ProductsBUndle\Model\ProductCatalogModel;
 
 /**
  * Class Product
@@ -29,6 +33,39 @@ use Contao\Config;
  */
 class ProductHelper
 {
+	/**
+	 * Sort out protected catalogs
+	 * @param array
+	 * @return array
+	 */
+	protected function sortOutProtected($arrCatalogs)
+	{
+		if (empty($arrCatalogs) || !\is_array($arrCatalogs))
+		{
+			return $arrCatalogs;
+		}
+
+		$objCatalog = ProductCatalogModel::findMultipleByIds($arrCatalogs);
+		$arrCatalogs = array();
+
+		if ($objCatalog !== null)
+		{
+			$security = System::getContainer()->get('security.helper');
+
+			while ($objCatalog->next())
+			{
+				if ($objCatalog->protected && !$security->isGranted(ContaoCorePermissions::MEMBER_IN_GROUPS, StringUtil::deserialize($objCatalog->groups, true)))
+				{
+					continue;
+				}
+
+				$arrCatalogs[] = $objCatalog->id;
+			}
+		}
+
+		return $arrCatalogs;
+	}
+
 
 	/**
 	 * Return the link of a product
