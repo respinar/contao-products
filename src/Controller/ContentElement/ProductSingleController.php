f96913fa617572233b652400d475242fc76c700a
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-06-22 00:00:19 +0330

    Delete unnecessary lines

--- src/Controller/ContentElement/ProductSingleController.php
+++ src/Controller/ContentElement/ProductSingleController.php
@@ -41,68 +41,11 @@ class ProductSingleController extends AbstractContentElementController
     {
 		$objProduct = ProductModel::findOneByID($model->product);
 
-        //$objPodcast = CatalogModel::findByIdOrAlias($objProduct->pid);
-
         $model->imgSize = $model->size;
 
         $template->product = Product::parseProduct($objProduct, $model);
 
         return $template->getResponse();
 	}
-
-	/**
-	 * Template
-	 * @var string
-	 */
-	// protected $strTemplate = 'ce_product';
-
-	/**
-	 * Display a wildcard in the back end
-	 * @return string
-	 */
-	// public function generate()
-	// {
-	// 	if (TL_MODE == 'BE')
-	// 	{
-	// 		$objTemplate = new \BackendTemplate('be_wildcard');
-
-	// 		$objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['product'][0]) . ' ###';
-
-	// 		$objProduct = ProductModel::findBy('id',$this->product);
-
-	// 		$objTemplate->title = $this->headline;
-	// 		$objTemplate->id = $objProduct->id;
-	// 		$objTemplate->link = $objProduct->title;
-	// 		$objTemplate->href = 'contao/main.php?do=catalogs&amp;table=tl_product&amp;act=edit&amp;id=' . $objProduct->id;
-
-	// 		$objFile = \FilesModel::findByUuid($objProduct->singleSRC);
-
-	// 		$objTemplate->singleSRC = $objFile->path;
-
-	// 		return $objTemplate->parse();
-	// 	}
-
-	// 	return parent::generate();
-	// }
-
-
-	/**
-	 * Generate the module
-	 */
-	// protected function compile()
-	// {
-
-	// 	$objProduct = ProductModel::findBy('id',$this->product);
-
-	// 	if (null === $objProduct)
-	// 	{
-	// 		echo "not found";
-	// 	}
-
-	// 	$arrProduct = $this->parseProduct($objProduct);
-
-	// 	$this->Template->product = $arrProduct;
-
-	// }
 
 }
