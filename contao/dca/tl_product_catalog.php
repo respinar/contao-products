f2311d3b695050638dfda9d41798a901d22cd761
Hamid Peywasti <hamid@respinar.com>
2023-09-07 19:16:23 +0330

    fix php 8 warning

--- contao/dca/tl_product_catalog.php
+++ contao/dca/tl_product_catalog.php
@@ -84,7 +84,7 @@ $GLOBALS['TL_DCA']['tl_product_catalog'] = array
 			(
 				'href'                => 'act=delete',
 				'icon'                => 'delete.svg',
-				'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"',
+				'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"',
 				'button_callback'     => array('tl_product_catalog', 'deleteCategory')
 			),
 			'show' => array
