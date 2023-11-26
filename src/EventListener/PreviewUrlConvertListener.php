516940b7e8820c0a2d319283d7a1c9339bea4336
Hamid Peywasti <hamid@respinar.com>
2023-11-26 16:26:03 +0330

    Add event listener for convert PreviewUrl

---
+++ src/EventListener/PreviewUrlConvertListener.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Products Bundle.
+ *
+ * (c) Hamid Peywasti 2023 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+namespace Respinar\ProductsBundle\EventListener;
+
+use Contao\CoreBundle\Event\PreviewUrlConvertEvent;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Respinar\ProductsBundle\Model\ProductModel;
+
+use Respinar\ProductsBundle\Controller\Product;
+
+
+#[AsEventListener('contao.preview_url_convert')]
+class PreviewUrlConvertListener
+{
+    private ContaoFramework  $framework;
+
+    public function __construct(ContaoFramework $framework)
+    {
+        $this->framework = $framework;
+    }
+
+    public function __invoke(PreviewUrlConvertEvent $event): void
+    {
+        // Do something
+        if (!$this->framework->isInitialized()) {
+            return;
+        }
+
+        if (null === ($product = $this->getProductModel($event->getRequest()))) {
+            return;
+        }
+
+        $event->setUrl($this->framework->getAdapter(Product::class)->generateProductUrl($product, false, true));
+
+    }
+
+    private function getProductModel(Request $request): ?ProductModel
+    {
+        if (!$request->query->has('product')) {
+            return null;
+        }
+
+        return $this->framework->getAdapter(ProductModel::class)->findByPk($request->query->get('product'));
+    }
+}
