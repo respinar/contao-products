e9624d6ddadbedb24cd9cf49b2bdd58b59848a0d
Hamid Peywasti <hamid@respinar.com>
2024-02-11 17:28:29 +0330

    Update license date

--- contao/models/CatalogModel.php
+++ contao/models/CatalogModel.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of Contao Products Bundle.
  *
- * (c) Hamid Peywasti 2023 <hamid@respinar.com>
+ * (c) Hamid Peywasti 2024 <hamid@respinar.com>
  *
  * @license MIT
  */
