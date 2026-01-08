4b10e06df670ca1f1b7042a5a544fc82cc6fc3cf
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-01-08 15:02:06 +0330

    remove extras

--- src/EventListener/DataContainer/ContentListener.php
+++ src/EventListener/DataContainer/ContentListener.php
@@ -19,8 +19,6 @@ class ContentListener
 {
     /**
      * Return all navigation templates as array.
-     *
-     * @return array
      */
     #[AsCallback(table: 'tl_content', target: 'fields.product_template.options')]
     public function getProductTemplates(): array
