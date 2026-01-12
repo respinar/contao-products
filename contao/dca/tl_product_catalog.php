2d02ef8281c66b627579b6eef8b486213469fe93
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-01-12 15:54:30 +0330

    convert arrya to []

--- contao/dca/tl_product_catalog.php
+++ contao/dca/tl_product_catalog.php
@@ -16,195 +16,166 @@ use Contao\Image;
 /**
  * Table tl_product_catalog
  */
-$GLOBALS['TL_DCA']['tl_product_catalog'] = array
-(
+$GLOBALS['TL_DCA']['tl_product_catalog'] = [
 
 	// Config
-	'config' => array
-	(
+	'config' => [
 		'dataContainer'               => DC_Table::class,
-		'ctable'                      => array('tl_product'),
+		'ctable'                      => ['tl_product'],
 		'enableVersioning'            => true,
-		'onload_callback' => array
-		(
-			array('tl_product_catalog', 'checkPermission')
-		),
-		'sql' => array
-		(
-			'keys' => array
-			(
+		'onload_callback' => [
+			['tl_product_catalog', 'checkPermission']
+		],
+		'sql' => [
+			'keys' => [
 				'id'    => 'primary'
-			)
-		)
-	),
+			]
+		]
+	],
 
 	// List
-	'list' => array
-	(
-		'sorting' => array
-		(
+	'list' => [
+		'sorting' => [
 			'mode'                    => 1,
-			'fields'                  => array('title'),
+			'fields'                  => ['title'],
 			'flag'                    => 1,
 			'panelLayout'             => 'filter;search,limit'
-		),
-		'label' => array
-		(
-			'fields'                  => array('title'),
+		],
+		'label' => [
+			'fields'                  => ['title'],
 			'format'                  => '%s'
-		),
-		'global_operations' => array
-		(
-			'all' => array
-			(
+		],
+		'global_operations' => [
+			'all' => [
 				'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
 				'href'                => 'act=select',
 				'class'               => 'header_edit_all',
 				'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
-			)
-		),
-		'operations' => array
-		(
+			]
+		],
+		'operations' => [
 			'edit',
 			'children' ,
 			'copy',
 			'delete',
 			'show'
-		)
-	),
+		]
+	],
 
 	// Palettes
-	'palettes' => array
-	(
-		'__selector__'                => array('protected', 'allowComments'),
+	'palettes' => [
+		'__selector__'                => ['protected', 'allowComments'],
 		'default'                     => '{title_legend},title;{redirect_legend},overviewPage,jumpTo;{protected_legend:hide},protected;{comments_legend:hide},allowComments'
-	),
+	],
 
 	// Subpalettes
-	'subpalettes' => array
-	(
+	'subpalettes' => [
 		'protected'                   => 'groups',
 		'allowComments'               => 'notify,sortOrder,perPage,moderate,bbcode,requireLogin,disableCaptcha'
-	),
+	],
 
 	// Fields
-	'fields' => array
-	(
-		'id' => array
-		(
+	'fields' => [
+		'id' => [
 			'sql'                     => "int(10) unsigned NOT NULL auto_increment"
-		),
-		'tstamp' => array
-		(
+		],
+		'tstamp' => [
 			'sql'                     => "int(10) unsigned NOT NULL default 0"
-		),
-		'title' => array
-		(
+		],
+		'title' => [
 			'exclude'                 => true,
 			'search'                  => true,
 			'inputType'               => 'text',
-			'eval'                    => array('mandatory'=>true, 'maxlength'=>128),
+			'eval'                    => ['mandatory'=>true, 'maxlength'=>128],
 			'sql'                     => "varchar(255) NOT NULL default ''"
-		),
-		'overviewPage' => array
-		(
+		],
+		'overviewPage' => [
 
 			'inputType'               => 'pageTree',
 			'foreignKey'              => 'tl_page.title',
-			'eval'                    => array('mandatory'=>true, 'fieldType'=>'radio'),
+			'eval'                    => ['mandatory'=>true, 'fieldType'=>'radio'],
 			'sql'                     => "int(10) unsigned NOT NULL default 0",
-			'relation'                => array('type'=>'hasOne', 'load'=>'lazy')
-		),
-		'jumpTo' => array
-		(
+			'relation'                => ['type'=>'hasOne', 'load'=>'lazy']
+		],
+		'jumpTo' => [
 
 			'inputType'               => 'pageTree',
 			'foreignKey'              => 'tl_page.title',
-			'eval'                    => array('mandatory'=>true, 'fieldType'=>'radio'),
+			'eval'                    => ['mandatory'=>true, 'fieldType'=>'radio'],
 			'sql'                     => "int(10) unsigned NOT NULL default 0",
-			'relation'                => array('type'=>'hasOne', 'load'=>'lazy')
-		),
-		'protected' => array
-		(
+			'relation'                => ['type'=>'hasOne', 'load'=>'lazy']
+		],
+		'protected' => [
 			'exclude'                 => true,
 			'inputType'               => 'checkbox',
-			'eval'                    => array('submitOnChange'=>true),
-			'sql'                     => array('type' => 'boolean', 'default' => false)
-		),
-		'groups' => array
-		(
+			'eval'                    => ['submitOnChange'=>true],
+			'sql'                     => ['type' => 'boolean', 'default' => false]
+		],
+		'groups' => [
 			'exclude'                 => true,
 			'inputType'               => 'checkbox',
 			'foreignKey'              => 'tl_member_group.name',
-			'eval'                    => array('mandatory'=>true, 'multiple'=>true),
+			'eval'                    => ['mandatory'=>true, 'multiple'=>true],
 			'sql'                     => "blob NULL",
-			'relation'                => array('type'=>'hasMany', 'load'=>'lazy')
-		),
-		'allowComments' => array
-		(
+			'relation'                => ['type'=>'hasMany', 'load'=>'lazy']
+		],
+		'allowComments' => [
 			'exclude'                 => true,
 			'filter'                  => true,
 			'inputType'               => 'checkbox',
-			'eval'                    => array('submitOnChange'=>true),
-			'sql'                     => array('type' => 'boolean', 'default' => false)
-		),
-		'notify' => array
-		(
+			'eval'                    => ['submitOnChange'=>true],
+			'sql'                     => ['type' => 'boolean', 'default' => false]
+		],
+		'notify' => [
 			'default'                 => 'notify_admin',
 			'exclude'                 => true,
 			'inputType'               => 'select',
-			'options'                 => array('notify_admin', 'notify_author', 'notify_both'),
-			'eval'                    => array('tl_class'=>'w50'),
+			'options'                 => ['notify_admin', 'notify_author', 'notify_both'],
+			'eval'                    => ['tl_class'=>'w50'],
 			'reference'               => &$GLOBALS['TL_LANG']['tl_product_catalog'],
 			'sql'                     => "varchar(16) NOT NULL default ''"
-		),
-		'sortOrder' => array
-		(
+		],
+		'sortOrder' => [
 			'default'                 => 'ascending',
 			'exclude'                 => true,
 			'inputType'               => 'select',
-			'options'                 => array('ascending', 'descending'),
+			'options'                 => ['ascending', 'descending'],
 			'reference'               => &$GLOBALS['TL_LANG']['MSC'],
-			'eval'                    => array('tl_class'=>'w50 clr'),
+			'eval'                    => ['tl_class'=>'w50 clr'],
 			'sql'                     => "varchar(32) NOT NULL default ''"
-		),
-		'perPage' => array
-		(
+		],
+		'perPage' => [
 			'exclude'                 => true,
 			'inputType'               => 'text',
-			'eval'                    => array('rgxp'=>'natural', 'tl_class'=>'w50'),
+			'eval'                    => ['rgxp'=>'natural', 'tl_class'=>'w50'],
 			'sql'                     => "smallint(5) unsigned NOT NULL default 0"
-		),
-		'moderate' => array
-		(
+		],
+		'moderate' => [
 			'exclude'                 => true,
 			'inputType'               => 'checkbox',
-			'eval'                    => array('tl_class'=>'w50'),
-			'sql'                     => array('type' => 'boolean', 'default' => false)
-		),
-		'bbcode' => array
-		(
+			'eval'                    => ['tl_class'=>'w50'],
+			'sql'                     => ['type' => 'boolean', 'default' => false]
+		],
+		'bbcode' => [
 			'exclude'                 => true,
 			'inputType'               => 'checkbox',
-			'eval'                    => array('tl_class'=>'w50'),
-			'sql'                     => array('type' => 'boolean', 'default' => false)
-		),
-		'requireLogin' => array
-		(
+			'eval'                    => ['tl_class'=>'w50'],
+			'sql'                     => ['type' => 'boolean', 'default' => false]
+		],
+		'requireLogin' => [
 			'exclude'                 => true,
 			'inputType'               => 'checkbox',
-			'eval'                    => array('tl_class'=>'w50'),
-			'sql'                     => array('type' => 'boolean', 'default' => false)
-		),
-		'disableCaptcha' => array
-		(
+			'eval'                    => ['tl_class'=>'w50'],
+			'sql'                     => ['type' => 'boolean', 'default' => false]
+		],
+		'disableCaptcha' => [
 			'exclude'                 => true,
 			'inputType'               => 'checkbox',
-			'eval'                    => array('tl_class'=>'w50'),
-			'sql'                     => array('type' => 'boolean', 'default' => false)
-		)
-	)
-);
+			'eval'                    => ['tl_class'=>'w50'],
+			'sql'                     => ['type' => 'boolean', 'default' => false]
+		]
+	]
+];
 
 
 class tl_product_catalog extends Backend
@@ -223,20 +194,13 @@ class tl_product_catalog extends Backend
 	* Check permissions to edit table tl_product_catalog
 	*/
 	public function checkPermission(): void
-    {
-    }
+  {
+  }
 
 	/**
      * Return the edit header button
-     *
-     * @param array  $row
-     * @param string $href
-     * @param string $label
-     * @param string $title
-     * @param string $icon
-     * @param string $attributes
      */
-    public function editHeader($row, $href, $label, $title, $icon, $attributes): void
+  public function editHeader(array $row, string $href, string $label, string $title, string $icon, string $attributes): void
 	{
 		// return $this->User->canEditFieldsOf('tl_product_catalog') ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ' : Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
 	}
@@ -244,15 +208,8 @@ class tl_product_catalog extends Backend
 
 	/**
      * Return the copy category button
-     *
-     * @param array  $row
-     * @param string $href
-     * @param string $label
-     * @param string $title
-     * @param string $icon
-     * @param string $attributes
      */
-    public function copyCategory($row, $href, $label, $title, $icon, $attributes): void
+  public function copyCategory(array $row, string $href, string $label, string $title, string $icon, string $attributes): void
 	{
 		// return $this->User->hasAccess('create', 'catalogp') ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ' : Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
 	}
@@ -260,15 +217,8 @@ class tl_product_catalog extends Backend
 
 	/**
      * Return the delete category button
-     *
-     * @param array  $row
-     * @param string $href
-     * @param string $label
-     * @param string $title
-     * @param string $icon
-     * @param string $attributes
      */
-    public function deleteCategory($row, $href, $label, $title, $icon, $attributes): void
+  public function deleteCategory(array $row, string $href, string $label, string $title, string $icon, string $attributes): void
 	{
 		// return $this->User->hasAccess('delete', 'catalogp') ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ' : Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
 	}
