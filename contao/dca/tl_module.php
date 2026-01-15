320124d125e66d8d0f2d3e4567feb8d9c6fe108a
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-01-15 16:08:16 +0330

    code cleanup

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -34,114 +34,113 @@ $GLOBALS['TL_DCA']['tl_module']['palettes']['product_detail']  = '
  * Add fields to tl_module
  */
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_catalogs'] = [
-	'exclude'              => true,
-	'inputType'            => 'checkbox',
-	'foreignKey'           => 'tl_product_catalog.title',
-	'eval'                 => ['multiple'=>true, 'mandatory'=>true],
-  'sql'                  => "blob NULL"
+	'exclude'    => true,
+	'inputType'  => 'checkbox',
+	'foreignKey' => 'tl_product_catalog.title',
+	'eval'       => ['multiple'=>true, 'mandatory' =>true],
+  'sql'        => "blob NULL"
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_categories'] = [
-  'exclude'                 => true,
-  'inputType'               => 'treePicker',
-  'foreignKey'              => 'tl_product_category.title',
-  'eval'                    => ['multiple'=>true, 'fieldType'=>'checkbox', 'foreignTable'=>'tl_product_category', 'titleField'=>'title', 'searchField'=>'title', 'managerHref'=>'table=tl_product_category'],
-  'sql'                     => "blob NULL"
+  'exclude'    => true,
+  'inputType'  => 'treePicker',
+  'foreignKey' => 'tl_product_category.title',
+  'eval'       => ['multiple'=>true, 'fieldType'=>'checkbox', 'foreignTable'=>'tl_product_category', 'titleField'=>'title', 'searchField'=>'title', 'managerHref'=>'table=tl_product_category'],
+  'sql'        => "blob NULL"
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_featured'] = [
-	'default'                 => 'all_product',
-	'exclude'                 => true,
-	'inputType'               => 'select',
-	'options'                 => ['all_product', 'featured_product', 'unfeatured_product'],
-	'reference'               => &$GLOBALS['TL_LANG']['tl_module'],
-	'eval'                    => ['tl_class'=>'w50'],
-	'sql'                     => "varchar(20) NOT NULL default ''"
+	'default'    => 'all_product',
+	'exclude'    => true,
+	'inputType'  => 'select',
+	'options'    => ['all_product', 'featured_product', 'unfeatured_product'],
+	'reference'  => &$GLOBALS['TL_LANG']['tl_module'],
+	'eval'       => ['tl_class'=>'w50'],
+	'sql'        => "varchar(20) NOT NULL default ''"
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_sortBy'] = [
-	'default'                 => 'custom',
-	'exclude'                 => true,
-	'inputType'               => 'select',
-	'options'                 => ['custom','date_desc', 'date_asc','title_asc', 'title_desc'],
-	'reference'               => &$GLOBALS['TL_LANG']['tl_module'],
-	'eval'                    => ['tl_class'=>'w50'],
-	'sql'                     => "varchar(16) NOT NULL default ''"
+	'default'    => 'custom',
+	'exclude'    => true,
+	'inputType'  => 'select',
+	'options'    => ['custom','date_desc', 'date_asc','title_asc', 'title_desc'],
+	'reference'  => &$GLOBALS['TL_LANG']['tl_module'],
+	'eval'       => ['tl_class'=>'w50'],
+	'sql'        => "varchar(16) NOT NULL default ''"
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_metaFields'] = [
-	'default'                 => [''],
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'options'                 => ['date','price','availability','brand','model','sku','global_ID','buy'],
-	'reference'               => &$GLOBALS['TL_LANG']['MSC'],
-	'eval'                    => ['multiple'=>true],
-	'sql'                     => "varchar(255) NOT NULL default ''"
+	'default'    => [''],
+	'exclude'    => true,
+	'inputType'  => 'checkbox',
+	'options'    => ['date','price','availability','brand','model','sku','global_ID','buy'],
+	'reference'  => &$GLOBALS['TL_LANG']['MSC'],
+	'eval'       => ['multiple' =>true],
+	'sql'        => "varchar(255) NOT NULL default ''"
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_detailModule'] = [
-	'exclude'                 => true,
-	'inputType'               => 'select',
-	'reference'               => &$GLOBALS['TL_LANG']['tl_module'],
-	'eval'                    => ['includeBlankOption'=>true, 'tl_class'=>'w50'],
-	'sql'                     => "int(10) unsigned NOT NULL default '0'"
+	'exclude'    => true,
+	'inputType'  => 'select',
+	'reference'  => &$GLOBALS['TL_LANG']['tl_module'],
+	'eval'       => ['includeBlankOption' =>true, 'tl_class'=>'w50'],
+	'sql'        => "int(10) unsigned NOT NULL default '0'"
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_template'] = [
-	'default'              => 'product_short',
-	'exclude'              => true,
-	'inputType'            => 'select',
-	'eval'                 => ['tl_class'=>'w50'],
-  'sql'                  => "varchar(64) NOT NULL default ''"
+	'default'    => 'product_short',
+	'exclude'    => true,
+	'inputType'  => 'select',
+	'eval'       => ['tl_class'=>'w50'],
+  'sql'        => "varchar(64) NOT NULL default ''"
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_listClass'] = [
-	'exclude'                 => true,
-	'inputType'               => 'text',
-	'eval'                    => ['maxlength'=>128, 'tl_class'=>'w50'],
-	'sql'                     => "varchar(255) NOT NULL default ''"
+	'exclude'    => true,
+	'inputType'  => 'text',
+	'eval'       => ['maxlength' =>128, 'tl_class'=>'w50'],
+	'sql'        => "varchar(255) NOT NULL default ''"
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_singleClass'] = [
-	'exclude'                 => true,
-	'inputType'               => 'text',
-	'eval'                    => ['maxlength'=>128, 'tl_class'=>'w50'],
-	'sql'                     => "varchar(255) NOT NULL default ''"
+	'exclude'    => true,
+	'inputType'  => 'text',
+	'eval'       => ['maxlength' =>128, 'tl_class'=>'w50'],
+	'sql'        => "varchar(255) NOT NULL default ''"
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_related'] = [
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'eval'                    => [],
-	'sql'                     => ['type' => 'boolean', 'default' => false]
+	'exclude'    => true,
+	'inputType'  => 'checkbox',
+	'eval'       => [],
+	'sql'        => ['type'  => 'boolean', 'default'  => false]
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_summary'] = [
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'eval'                    => ['tl_class'=>'w50 m12'],
-	'sql'                     => ['type' => 'boolean', 'default' => false]
+	'exclude'    => true,
+	'inputType'  => 'checkbox',
+	'eval'       => ['tl_class'=>'w50 m12'],
+	'sql'        => ['type'  => 'boolean', 'default'  => false]
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_relatedTpl'] = [
-	'default'              => 'product_related',
-	'exclude'              => true,
-	'inputType'            => 'select',
-	'eval'                 => ['tl_class'=>'w50'],
-  'sql'                  => "varchar(64) NOT NULL default ''"
+	'default'    => 'product_related',
+	'exclude'    => true,
+	'inputType'  => 'select',
+	'eval'       => ['tl_class'=>'w50'],
+  'sql'        => "varchar(64) NOT NULL default ''"
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_relatedImgSize'] = [
-	'exclude'                 => true,
-	'inputType'               => 'imageSize',
-	'reference'               => &$GLOBALS['TL_LANG']['MSC'],
+	'exclude'    => true,
+	'inputType'  => 'imageSize',
+	'reference'  => &$GLOBALS['TL_LANG']['MSC'],
 	'options_callback' => function () {
 		return System::getContainer()->get('contao.image.sizes')->getOptionsForUser(BackendUser::getInstance());
 	},
-	'eval'                    => ['rgxp'=>'natural', 'includeBlankOption'=>true, 'nospace'=>true, 'helpwizard'=>true, 'tl_class'=>'w50'],
-	'sql'                     => "varchar(128) COLLATE ascii_bin NOT NULL default ''"
+	'eval'       => ['rgxp'=>'natural', 'includeBlankOption' =>true, 'nospace' =>true, 'helpwizard' =>true, 'tl_class'=>'w50'],
+	'sql'        => "varchar(128) COLLATE ascii_bin NOT NULL default ''"
 ];
 $GLOBALS['TL_DCA']['tl_module']['fields']['product_comHeadline'] = [
-	'exclude'                 => true,
-	'search'                  => true,
-	'inputType'               => 'inputUnit',
-	'options'                 => ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
-	'eval'                    => ['maxlength'=>200, 'tl_class'=>'w50 clr'],
-	'sql'                     => "varchar(255) NOT NULL default 'a:2:{s:5:\"value\";s:0:\"\";s:4:\"unit\";s:2:\"h2\";}'"
+	'exclude'    => true,
+	'search'     => true,
+	'inputType'  => 'inputUnit',
+	'options'    => ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+	'eval'       => ['maxlength' =>200, 'tl_class'=>'w50 clr'],
+	'sql'        => "varchar(255) NOT NULL default 'a:2:{s:5:\"value\";s:0:\"\";s:4:\"unit\";s:2:\"h2\";}'"
 ];
 
 $bundles = System::getContainer()->getParameter('kernel.bundles');
 
 // Add the comments template drop-down menu
-if (isset($bundles['ContaoCommentsBundle']))
-{
+if (isset($bundles['ContaoCommentsBundle'])) {
 	$GLOBALS['TL_DCA']['tl_module']['palettes']['product_detail'] = str_replace('{protected_legend:hide}', '{comment_legend:hide},product_comHeadline,com_template;{protected_legend:hide}', $GLOBALS['TL_DCA']['tl_module']['palettes']['product_detail']);
 }
