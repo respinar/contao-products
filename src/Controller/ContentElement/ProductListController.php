0b0e75841612a932f4c3c100a75fe0ae372b9f4c
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-01-27 19:40:55 +0330

    make twig compitable

--- src/Controller/ContentElement/ProductListController.php
+++ src/Controller/ContentElement/ProductListController.php
@@ -29,7 +29,7 @@ use Respinar\ProductsBundle\Product;
 use Respinar\ProductsBundle\Model\ProductModel;
 
 
-#[AsContentElement(category: "products", template: 'ce_product_list')]
+#[AsContentElement(category: "products")]
 class ProductListController extends AbstractContentElementController
 {
 
