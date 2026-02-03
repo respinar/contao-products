230d1cad7633e818aa38b6d549980ed7fcdecd40
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-02-03 15:47:36 +0330

    use UrlGenerator class

--- src/EventListener/InsertTagsListener.php
+++ src/EventListener/InsertTagsListener.php
@@ -15,47 +15,64 @@ namespace Respinar\ProductsBundle\EventListener;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Psr\Log\LoggerInterface;
-
 use Respinar\ProductsBundle\Model\ProductModel;
-use Respinar\ProductsBundle\Product;
+use Respinar\ProductsBundle\Product\UrlGenerator;
 
 #[AsHook('replaceInsertTags')]
 class InsertTagsListener
 {
-    public const SUPPORTED_TAGS = [
-        'product_url'
+    private const SUPPORTED_TAGS = [
+        'product_url',
     ];
 
-    public function __construct(private readonly ContaoFramework $framework, private readonly LoggerInterface $logger)
-    {
+    public function __construct(
+        private readonly ContaoFramework $framework,
+        private readonly LoggerInterface $logger,
+    ) {
     }
 
-    public function __invoke(string $tag, bool $useCache, $cacheValue, array $flags): string|false
-    {
+    public function __invoke(
+        string $tag,
+        bool $useCache,
+        mixed $cacheValue,
+        array $flags,
+    ): string|false {
         $elements = explode('::', $tag);
         $key = strtolower($elements[0]);
 
-        if (\in_array($key, self::SUPPORTED_TAGS, true)) {
-            return $this->replaceProductInsertTags($key, $elements[1], [...$flags, ...\array_slice($elements, 2)]);
+        if (!\in_array($key, self::SUPPORTED_TAGS, true)) {
+            return false;
         }
 
-        return false;
+        return $this->replaceProductInsertTags(
+            $key,
+            $elements[1] ?? '',
+            [...$flags, ...\array_slice($elements, 2)],
+        );
     }
 
-    private function replaceProductInsertTags (string $insertTag, string $idOrAlias, array $arguments): string
-    {
+    private function replaceProductInsertTags(
+        string $insertTag,
+        string $idOrAlias,
+        array $arguments,
+    ): string {
         $this->framework->initialize();
 
-        $adapter = $this->framework->getAdapter(ProductModel::class);
+        $productModel = $this->framework
+            ->getAdapter(ProductModel::class)
+            ->findByIdOrAlias($idOrAlias);
 
-        if (!$model = $adapter->findByIdOrAlias($idOrAlias)) {
+        if (null === $productModel) {
             return '';
         }
 
-        $product = $this->framework->getAdapter(Product::class);
-
         return match ($insertTag) {
-            'product_url' => $product->generateProductUrl($model, false, \in_array('absolute', $arguments, true)) ?: './',
+            'product_url' => UrlGenerator::generate(
+                $productModel,
+                false,
+                \in_array('absolute', $arguments, true),
+            ) ?: './',
+
             default => '',
         };
     }
