af9a66c20063d039f46c7617930dcf73392746d6
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-02-20 23:44:23 +0330

    cleanup and format

--- contao/dca/tl_user.php
+++ contao/dca/tl_user.php
@@ -8,30 +8,34 @@
  * @license MIT
  */
 
-
 /**
  * Extend default palette
  */
-$GLOBALS['TL_DCA']['tl_user']['palettes']['extend'] = str_replace('formp;', 'formp;{product_legend},products,productp;', $GLOBALS['TL_DCA']['tl_user']['palettes']['extend']);
-$GLOBALS['TL_DCA']['tl_user']['palettes']['custom'] = str_replace('formp;', 'formp;{product_legend},products,productp;', $GLOBALS['TL_DCA']['tl_user']['palettes']['custom']);
-
+$GLOBALS["TL_DCA"]["tl_user"]["palettes"]["extend"] = str_replace(
+  "formp;",
+  "formp;{product_legend},products,productp;",
+  $GLOBALS["TL_DCA"]["tl_user"]["palettes"]["extend"],
+);
+$GLOBALS["TL_DCA"]["tl_user"]["palettes"]["custom"] = str_replace(
+  "formp;",
+  "formp;{product_legend},products,productp;",
+  $GLOBALS["TL_DCA"]["tl_user"]["palettes"]["custom"],
+);
 
 /**
  * Add fields to tl_user_group
  */
-$GLOBALS['TL_DCA']['tl_user']['fields']['products'] = [
-	'exclude'    => true,
-	'inputType'  => 'checkbox',
-	'foreignKey' => 'tl_product_catalog.title',
-	'eval'       => ['multiple'=>true],
-	'sql'        => "blob NULL"
+$GLOBALS["TL_DCA"]["tl_user"]["fields"]["products"] = [
+  "inputType" => "checkbox",
+  "foreignKey" => "tl_product_catalog.title",
+  "eval" => ["multiple" => true],
+  "sql" => "blob NULL",
 ];
 
-$GLOBALS['TL_DCA']['tl_user']['fields']['productp'] = [
-	'exclude'    => true,
-	'inputType'  => 'checkbox',
-	'options'    => ['create', 'delete'],
-	'reference'  => &$GLOBALS['TL_LANG']['MSC'],
-	'eval'       => ['multiple'=>true],
-	'sql'        => "blob NULL"
+$GLOBALS["TL_DCA"]["tl_user"]["fields"]["productp"] = [
+  "inputType" => "checkbox",
+  "options" => ["create", "delete"],
+  "reference" => &$GLOBALS["TL_LANG"]["MSC"],
+  "eval" => ["multiple" => true],
+  "sql" => "blob NULL",
 ];
