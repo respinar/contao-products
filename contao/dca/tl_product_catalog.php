beb68a0c57b27d700a1ac788049b8da808f0b8b7
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-02-24 12:52:16 +0330

    fixed by rector

--- contao/dca/tl_product_catalog.php
+++ contao/dca/tl_product_catalog.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Products Bundle.
  *
@@ -11,7 +13,6 @@
 use Contao\Backend;
 use Contao\BackendUser;
 use Contao\DC_Table;
-use Contao\Image;
 
 use Respinar\ProductsBundle\Dca\CommentFields;
 
