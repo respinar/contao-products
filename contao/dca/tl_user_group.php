08428dd9a09ab4c13eee3c22183878ba201cc1e0
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-02-25 18:28:40 +0330

    format by ecs

--- contao/dca/tl_user_group.php
+++ contao/dca/tl_user_group.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Products Bundle.
  *
@@ -8,29 +10,27 @@
  * @license MIT
  */
 
-
-/**
+/*
  * Extend default palette
  */
 $GLOBALS['TL_DCA']['tl_user_group']['palettes']['default'] = str_replace('formp;', 'formp;{product_legend},products,productp;', $GLOBALS['TL_DCA']['tl_user_group']['palettes']['default']);
 
-
-/**
+/*
  * Add fields to tl_user_group
  */
 $GLOBALS['TL_DCA']['tl_user_group']['fields']['products'] = [
-	'exclude'    => true,
-	'inputType'  => 'checkbox',
-	'foreignKey' => 'tl_product_catalog.title',
-	'eval'       => ['multiple'=>true],
-	'sql'        => "blob NULL"
+    'exclude' => true,
+    'inputType' => 'checkbox',
+    'foreignKey' => 'tl_product_catalog.title',
+    'eval' => ['multiple' => true],
+    'sql' => 'blob NULL',
 ];
 
 $GLOBALS['TL_DCA']['tl_user_group']['fields']['productp'] = [
-	'exclude'    => true,
-	'inputType'  => 'checkbox',
-	'options'    => ['create', 'delete'],
-	'reference'  => &$GLOBALS['TL_LANG']['MSC'],
-	'eval'       => ['multiple'=>true],
-	'sql'        => "blob NULL"
+    'exclude' => true,
+    'inputType' => 'checkbox',
+    'options' => ['create', 'delete'],
+    'reference' => &$GLOBALS['TL_LANG']['MSC'],
+    'eval' => ['multiple' => true],
+    'sql' => 'blob NULL',
 ];
