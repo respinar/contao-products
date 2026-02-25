050df97310a48d43ffd53cede67da102c20f6194
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-02-25 18:28:50 +0330

    format by ecs

--- contao/dca/tl_user.php
+++ contao/dca/tl_user.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Products Bundle.
  *
@@ -8,34 +10,34 @@
  * @license MIT
  */
 
-/**
+/*
  * Extend default palette
  */
-$GLOBALS["TL_DCA"]["tl_user"]["palettes"]["extend"] = str_replace(
-  "formp;",
-  "formp;{product_legend},products,productp;",
-  $GLOBALS["TL_DCA"]["tl_user"]["palettes"]["extend"],
+$GLOBALS['TL_DCA']['tl_user']['palettes']['extend'] = str_replace(
+    'formp;',
+    'formp;{product_legend},products,productp;',
+    $GLOBALS['TL_DCA']['tl_user']['palettes']['extend'],
 );
-$GLOBALS["TL_DCA"]["tl_user"]["palettes"]["custom"] = str_replace(
-  "formp;",
-  "formp;{product_legend},products,productp;",
-  $GLOBALS["TL_DCA"]["tl_user"]["palettes"]["custom"],
+$GLOBALS['TL_DCA']['tl_user']['palettes']['custom'] = str_replace(
+    'formp;',
+    'formp;{product_legend},products,productp;',
+    $GLOBALS['TL_DCA']['tl_user']['palettes']['custom'],
 );
 
-/**
+/*
  * Add fields to tl_user_group
  */
-$GLOBALS["TL_DCA"]["tl_user"]["fields"]["products"] = [
-  "inputType" => "checkbox",
-  "foreignKey" => "tl_product_catalog.title",
-  "eval" => ["multiple" => true],
-  "sql" => "blob NULL",
+$GLOBALS['TL_DCA']['tl_user']['fields']['products'] = [
+    'inputType' => 'checkbox',
+    'foreignKey' => 'tl_product_catalog.title',
+    'eval' => ['multiple' => true],
+    'sql' => 'blob NULL',
 ];
 
-$GLOBALS["TL_DCA"]["tl_user"]["fields"]["productp"] = [
-  "inputType" => "checkbox",
-  "options" => ["create", "delete"],
-  "reference" => &$GLOBALS["TL_LANG"]["MSC"],
-  "eval" => ["multiple" => true],
-  "sql" => "blob NULL",
+$GLOBALS['TL_DCA']['tl_user']['fields']['productp'] = [
+    'inputType' => 'checkbox',
+    'options' => ['create', 'delete'],
+    'reference' => &$GLOBALS['TL_LANG']['MSC'],
+    'eval' => ['multiple' => true],
+    'sql' => 'blob NULL',
 ];
