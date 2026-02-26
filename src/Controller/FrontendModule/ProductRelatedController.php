be291e6473d5d148e0d86a8e9e1d7d46601079f7
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-02-26 18:30:05 +0330

    format by ecs

--- src/Controller/FrontendModule/ProductRelatedController.php
+++ src/Controller/FrontendModule/ProductRelatedController.php
@@ -10,73 +10,66 @@ declare(strict_types=1);
  * @license MIT
  */
 
-
 /**
- * Namespace
+ * Namespace.
  */
+
 namespace Respinar\ProductsBundle\Controller\FrontendModule;
 
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
-use Contao\ModuleModel;
-use Contao\Template;
 use Contao\Input;
+use Contao\ModuleModel;
 use Contao\PageModel;
 use Contao\StringUtil;
+use Contao\Template;
+use Respinar\ProductsBundle\Model\ProductModel;
+use Respinar\ProductsBundle\Product\ProductParser;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-use Respinar\ProductsBundle\Product\ProductParser;
-use Respinar\ProductsBundle\Model\ProductModel;
-
-
-#[AsFrontendModule(category: "products")]
+#[AsFrontendModule(category: 'products')]
 class ProductRelatedController extends AbstractFrontendModuleController
 {
-	public const TYPE = 'product_related';
+    public const TYPE = 'product_related';
 
-	public function __construct(
-      private readonly ProductParser $productParser,
-  ) {
-  }
+    public function __construct(private readonly ProductParser $productParser)
+    {
+    }
 
-	protected function getResponse(Template $template, ModuleModel $model, Request $request): Response
-	{
-		// Return an empty string if "auto_item" is not set to combine list and reader on same page
-		if (Input::get('auto_item') === null)
-		{
-			return '';
-		}
+    protected function getResponse(Template $template, ModuleModel $model, Request $request): Response
+    {
+        // Return an empty string if "auto_item" is not set to combine list and reader on
+        // same page
+        if (null === Input::get('auto_item')) {
+            return '';
+        }
 
-		$objProduct = ProductModel::findPublishedByIdOrAlias(Input::get('auto_item'));
+        $objProduct = ProductModel::findPublishedByIdOrAlias(Input::get('auto_item'));
 
-		if (null === $objProduct)
-		{
-			return '';
-		}
+        if (null === $objProduct) {
+            return '';
+        }
 
-		$template->referer = PageModel::findById($objProduct->getRelated('pid')->overviewPage)->getFrontendUrl();
+        $template->referer = PageModel::findById($objProduct->getRelated('pid')->overviewPage)->getFrontendUrl();
 
-		if ($model->overviewPage)
-		{
-			$template->referer = PageModel::findById($model->overviewPage)->getFrontendUrl();
-		}
+        if ($model->overviewPage) {
+            $template->referer = PageModel::findById($model->overviewPage)->getFrontendUrl();
+        }
 
-		$template->back = $model->customLabel ?: $GLOBALS['TL_LANG']['MSC']['newsOverview'];
-		$template->relateds_headline = $GLOBALS['TL_LANG']['MSC']['relateds_headline'];
+        $template->back = $model->customLabel ?: $GLOBALS['TL_LANG']['MSC']['newsOverview'];
+        $template->relateds_headline = $GLOBALS['TL_LANG']['MSC']['relateds_headline'];
 
-		$relatedIds = StringUtil::deserialize($objProduct->related);
+        $relatedIds = StringUtil::deserialize($objProduct->related);
 
-		if ($relatedIds)
-		{
-			$objRelated = ProductModel::findPublishedByIds($relatedIds);
+        if ($relatedIds) {
+            $objRelated = ProductModel::findPublishedByIds($relatedIds);
 
-			if ($objRelated !== null)
-			{
-				$template->relateds = $this->productParser->parseProducts($objRelated, $model);
-			}
-		}
+            if (null !== $objRelated) {
+                $template->relateds = $this->productParser->parseProducts($objRelated, $model);
+            }
+        }
 
-		return $template->getResponse();
-	}	
+        return $template->getResponse();
+    }
 }
