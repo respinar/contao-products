e3ff19149c69351f920590ef88449f83767184c1
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-02 18:31:13 +0330

    format by ecs

--- src/EventListener/DataContainer/ModuleListener.php
+++ src/EventListener/DataContainer/ModuleListener.php
@@ -11,18 +11,14 @@ declare(strict_types=1);
  */
 
 namespace Respinar\ProductsBundle\EventListener\DataContainer;
-use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
 
 use Contao\Backend;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
 use Contao\Database;
 
 class ModuleListener
 {
-	// public function __construct
-    // (
-    //     private readonly Database $Datebase
-    // ){
-    // }
+    // public function __construct (     private readonly Database $Datebase ){ }
     /**
      * Return all navigation templates as array.
      */
@@ -33,28 +29,27 @@ class ModuleListener
     }
 
     /**
-     * Return all related templates as array
+     * Return all related templates as array.
      */
     #[AsCallback(table: 'tl_module', target: 'fields.product_relatedTpl.options')]
-	public function getRelatedTemplates(): array
-	{
-		return Backend::getTemplateGroup('product_');
-	}
+    public function getRelatedTemplates(): array
+    {
+        return Backend::getTemplateGroup('product_');
+    }
 
-	/**
-     * Get all product detail modules and return them as array
+    /**
+     * Get all product detail modules and return them as array.
      */
     #[AsCallback(table: 'tl_module', target: 'fields.product_detailModule.options')]
-	public function getDetailModules(): array
-	{
-		$arrModules = array();
-		$objModules = Database::getInstance()->execute("SELECT m.id, m.name, t.name AS theme FROM tl_module m LEFT JOIN tl_theme t ON m.pid=t.id WHERE m.type='product_detail' ORDER BY t.name, m.name");
+    public function getDetailModules(): array
+    {
+        $arrModules = [];
+        $objModules = Database::getInstance()->execute("SELECT m.id, m.name, t.name AS theme FROM tl_module m LEFT JOIN tl_theme t ON m.pid=t.id WHERE m.type='product_detail' ORDER BY t.name, m.name");
 
-		while ($objModules->next())
-		{
-			$arrModules[$objModules->theme][$objModules->id] = $objModules->name . ' (ID ' . $objModules->id . ')';
-		}
+        while ($objModules->next()) {
+            $arrModules[$objModules->theme][$objModules->id] = $objModules->name.' (ID '.$objModules->id.')';
+        }
 
-		return $arrModules;
-	}
+        return $arrModules;
+    }
 }
