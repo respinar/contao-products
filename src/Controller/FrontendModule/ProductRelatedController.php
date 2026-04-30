37b3463223f3e778eedd71509afb0d7fd0db0d22
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-04-30 17:23:29 +0330

    Migrate controllers to Contao FragmentTemplate

--- src/Controller/FrontendModule/ProductRelatedController.php
+++ src/Controller/FrontendModule/ProductRelatedController.php
@@ -10,24 +10,20 @@ declare(strict_types=1);
  * @license MIT
  */
 
-/**
- * Namespace.
- */
-
 namespace Respinar\ProductsBundle\Controller\FrontendModule;
 
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
+use Contao\CoreBundle\Twig\FragmentTemplate;
 use Contao\ModuleModel;
 use Contao\PageModel;
 use Contao\StringUtil;
-use Contao\Template;
 use Respinar\ProductsBundle\Model\ProductModel;
 use Respinar\ProductsBundle\Product\ProductParser;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-#[AsFrontendModule(category: 'products')]
+#[AsFrontendModule(category: 'products', template: 'frontend_module/product_related')]
 class ProductRelatedController extends AbstractFrontendModuleController
 {
     public const TYPE = 'product_related';
@@ -36,20 +32,20 @@ class ProductRelatedController extends AbstractFrontendModuleController
     {
     }
 
-    protected function getResponse(Template $template, ModuleModel $model, Request $request): Response
+    protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response
     {
         $autoItem = $request->attributes->get('auto_item') ?? $request->query->get('auto_item');
 
-        // Return an empty string if "auto_item" is not set to combine list and reader on
-        // same page
+        // Return an empty response if "auto_item" is not set to combine list and reader
+        // on same page
         if (null === $autoItem) {
-            return '';
+            return new Response('');
         }
 
         $objProduct = ProductModel::findPublishedByIdOrAlias($autoItem);
 
         if (null === $objProduct) {
-            return '';
+            return new Response('');
         }
 
         $template->referer = PageModel::findById($objProduct->getRelated('pid')->overviewPage)->getFrontendUrl();
