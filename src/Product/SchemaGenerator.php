df6c13f9c38da905107a58afc6a094573cf21b49
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-05-28 23:25:34 +0330

    Refactor SchemaGenerator for ID and conditional fields

--- src/Product/SchemaGenerator.php
+++ src/Product/SchemaGenerator.php
@@ -13,49 +13,77 @@ declare(strict_types=1);
 namespace Respinar\ProductsBundle\Product;
 
 use Contao\StringUtil;
-use Contao\System;
+use Contao\CoreBundle\Routing\ContentUrlGenerator;
+use Contao\CoreBundle\String\HtmlDecoder;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 final class SchemaGenerator
 {
+    public function __construct(
+        private readonly HtmlDecoder $htmlDecoder,
+        private readonly ContentUrlGenerator $contentUrlGenerator,
+    ) {
+    }
+
     /**
      * Return the schema.org data from a product.
      */
     public function generate(object $product): array
     {
-        $htmlDecoder = System::getContainer()->get('contao.string.html_decoder');
+        $jsonLd = [
+            '@context' => 'https://schema.org',
+            '@type' => 'Product',
+            '@id' => $this->contentUrlGenerator->generate($product, [], UrlGeneratorInterface::ABSOLUTE_URL).'#/schema/product/'.$product->id,
+            'name' => $this->htmlDecoder->inputEncodedToPlainText((string) $product->title),
+        ];
 
-        $price = StringUtil::deserialize($product->price);
+        if ($product->description) {
+            $jsonLd['description'] = $product->description;
+        }
+
+        if ($product->sku) {
+            $jsonLd['sku'] = $product->sku;
+        }
+
         $globalId = StringUtil::deserialize($product->global_ID);
 
-        $jsonLd = [
-            '@type' => 'Product',
-            'name' => $htmlDecoder->inputEncodedToPlainText($product->title),
-            'description' => $product->description,
-            'sku' => $product->sku,
-            $globalId['unit'] => $globalId['value'],
-            'brand' => [
+        if (isset($globalId['unit'], $globalId['value']) && '' !== $globalId['value']) {
+            $jsonLd[$globalId['unit']] = $globalId['value'];
+        }
+
+        if ($product->brand) {
+            $jsonLd['brand'] = [
                 '@type' => 'Brand',
                 'name' => $product->brand,
-            ],
-        ];
+            ];
+        }
 
-        $jsonLd['aggregateRating'] = [
-            '@type' => 'AggregateRating',
-            'ratingValue' => $product->rating_value,
-            'reviewCount' => $product->rating_count,
-        ];
+        if (null !== $product->rating_value && null !== $product->rating_count) {
+            $jsonLd['aggregateRating'] = [
+                '@type' => 'AggregateRating',
+                'ratingValue' => $product->rating_value,
+                'reviewCount' => $product->rating_count,
+            ];
+        }
 
-        $jsonLd['offers'] = [
-            '@type' => 'Offer',
-            'priceCurrency' => $price['unit'],
-            'price' => $price['value'],
-            'priceValidUntil' => date('Y-m-d\TH:i:sP', $product->priceValidUntil),
-            'availability' => 'http://schema.org/'.$product->availability,
-            'seller' => [
-                '@type' => 'Organization',
-                'name' => $product->brand,
-            ],
-        ];
+        $price = StringUtil::deserialize($product->price);
+
+        if (isset($price['unit'], $price['value']) && '' !== $price['value']) {
+            $jsonLd['offers'] = [
+                '@type' => 'Offer',
+                'priceCurrency' => $price['unit'],
+                'price' => $price['value'],
+                'priceValidUntil' => date('Y-m-d\TH:i:sP', (int) $product->priceValidUntil),
+                'availability' => 'https://schema.org/'.$product->availability,
+            ];
+
+            if ($product->brand) {
+                $jsonLd['offers']['seller'] = [
+                    '@type' => 'Organization',
+                    'name' => $product->brand,
+                ];
+            }
+        }
 
         return $jsonLd;
     }
